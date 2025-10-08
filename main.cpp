3939105a1e6ea646d1704119f3390940070f5c64
MahmoudElshrif <mahmoudfathyelshrif@gmail.com>
2025-10-08 22:45:30 +0300

    draw into buffer

--- main.cpp
+++ main.cpp
@@ -1,29 +1,34 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 
 using namespace std;
 
-int width = 100;
-int height = 30;
+int width = 150;
+int height = 50;
 int refreshtick = 0;
 int refreshtickmax = 1000;
 
 string *buffer;
 
+void clear_buffer(string *buffer)
+{
+	for (int i = 0; i < height; i++)
+	{
+		string s = string(width, ' ');
+		buffer[i] = s;
+	}
+}
 void init_buffer()
 {
 	buffer = new string[height];
-	for (int i = 0; i < height; i++)
-	{
-		string s = string(width, 'h');
-		buffer[i] = s;
-	}
+	clear_buffer(buffer);
 }
 
 bool valid_position(int x, int y)
 {
 	if (x < 0 || y < 0 ||
-		x >= width || y >= height)
+		x > width || y > height)
 	{
 		return false;
 	}
@@ -70,6 +75,14 @@ void render_buffer(string *buffer)
 
 void draw_circle(string *buffer, int x, int y, int rad)
 {
+	for (int xf = max(0, x - rad); xf < min(width, x + rad + 1); xf++)
+	{
+		for (int yf = max(0, y - rad); yf < min(height, y + rad + 1); yf++)
+		{
+			if ((yf - y) * (yf - y) + (xf - x) * (xf - x) <= rad * rad)
+				set_cell(buffer, xf, yf, 'f');
+		}
+	}
 }
 
 int main()
@@ -77,8 +90,12 @@ int main()
 	std::ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	init_buffer();
+	double x = 0;
 	while (true)
 	{
+		clear_buffer(buffer);
+		x += 0.0005;
+		draw_circle(buffer, cos(x) * 10 + width / 2, sin(x) * 4 + height / 2, 4);
 		render_buffer(buffer);
 	}
 
